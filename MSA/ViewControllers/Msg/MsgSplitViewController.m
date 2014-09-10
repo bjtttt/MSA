d02dc4b7d8e30ea612c83f54570dfee4f34d502e
GUO Zhitao <guozhitaobj@163.com>
2014-09-10 22:07:55 +0800

    CC

---
+++ MSA/ViewControllers/Msg/MsgSplitViewController.m
@@ -0,0 +1,30 @@
+//
+//  MsgSplitViewController.m
+//  MSA
+//
+//  Created by guozhitao on 9/10/14.
+//  Copyright (c) 2014 KeySight. All rights reserved.
+//
+
+#import "ShareSettings.h"
+#import "MsgSplitViewController.h"
+
+@interface MsgSplitViewController ()
+
+@end
+
+@implementation MsgSplitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.msgMasterNVC=[self.viewControllers lastObject];
+    self.msgDetaillCVC=[self.viewControllers firstObject];
+    
+    self.msgMasterNVC.msgDetaillCVC=self.msgDetaillCVC;
+}
+
+
+
+@end
