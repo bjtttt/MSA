0235f680562d56533cf8b7bd4650760457d08804
GUO Zhitao <bjguozhitao@icloud.com>
2014-10-30 22:38:10 +0800

    Add Soft Menu - Not Completed Yet

---
+++ MSA/ViewControllers/Menu/Soft/CellStyle/SoftMenuLeftLabelResultBooleanBranchTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  SoftMenuLeftLabelResultBooleanBranchTableViewCell.h
+//  MSA
+//
+//  Created by GUO Zhitao on 22/09/14.
+//  Copyright (c) 2014 KeySight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ShareSettings.h"
+
+@interface SoftMenuLeftLabelResultBooleanBranchTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UILabel *content;
+@property (weak, nonatomic) IBOutlet UISwitch *state;
+
+@end
---
+++ MSA/ViewControllers/Menu/Soft/CellStyle/SoftMenuLeftLabelResultBooleanBranchTableViewCell.m
@@ -0,0 +1,22 @@
+//
+//  SoftMenuLeftLabelResultBooleanBranchTableViewCell.m
+//  MSA
+//
+//  Created by GUO Zhitao on 22/09/14.
+//  Copyright (c) 2014 KeySight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SoftMenuLeftLabelResultBooleanBranchTableViewCell.h"
+
+@interface SoftMenuLeftLabelResultBooleanBranchTableViewCell()
+
+@end
+
+@implementation SoftMenuLeftLabelResultBooleanBranchTableViewCell
+
+//-(void)viewDidLoad{
+//    [super viewDidLoad];
+//}
+
+@end
