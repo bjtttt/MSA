1fd62cf681bd6a1f04af330ec6647de608a89d06
GUO Zhitao <bjguozhitao@icloud.com>
2014-11-04 18:15:08 +0800

    Measure Bar Index 2 OK

---
+++ MSA/ViewControllers/Bar/CellStyle/Bar0OneLineYellowTableViewCell.m
@@ -0,0 +1,22 @@
+//
+//  Bar0OneLineYellowTableViewCell.m
+//  MSA
+//
+//  Created by GUO Zhitao on 22/09/14.
+//  Copyright (c) 2014 KeySight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Bar0OneLineYellowTableViewCell.h"
+
+@interface Bar0OneLineYellowTableViewCell()
+
+@end
+
+@implementation Bar0OneLineYellowTableViewCell
+
+//-(void)viewDidLoad{
+//    [super viewDidLoad];
+//}
+
+@end
