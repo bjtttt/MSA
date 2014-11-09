e3bdfb2553208bceae313e8534cc95f3bb48597e
GUO Zhitao <bjguozhitao@icloud.com>
2014-11-09 22:04:55 +0800

    Create Par Dict

--- MSA/Mode/Parameter/MeasureBar/MeasureBarParameterCollection.m
+++ MSA/Mode/Parameter/MeasureBar/MeasureBarParameterCollection.m
@@ -8,6 +8,10 @@
 
 #import <Foundation/Foundation.h>
 #import "MeasureBarParameterCollection.h"
+#import "UISoftMenu.h"
+#import "UISoftPanel.h"
+#import "UISoftKey.h"
+#import "ShareSettings.h"
 
 @interface MeasureBarParameterCollection()
 
@@ -48,7 +52,37 @@
 
 -(void)parseParameter
 {
-    
+    UISoftMenu *sMenu = self.shareSettings.softMenuSystem;
+    NSMutableArray *mbarSystem = sMenu.measBarPanels;
+    int count = mbarSystem.count;
+    for(int i=0;i<count;i++)
+    {
+        UISoftPanel *sPanel=[mbarSystem objectAtIndex:i];
+        NSMutableArray *sKeyArray=sPanel.keyArray;
+        int countKeys=sKeyArray.count;
+        for(int j=0;j<countKeys;j++)
+        {
+            UISoftKey *sKey=[sKeyArray objectAtIndex:j];
+            NSMutableString *nameString=sKey.nameString;
+            ValueType vType=sKey.valueType;
+            switch(sKey.valueType)
+            {
+                case VAL_IMM:
+                    break;
+                case VAL_STRING:
+                    break;
+                case VAL_INT:
+                case VAL_DOUBLE:
+                case VAL_BOOL_AUTOMAN:
+                case VAL_BOOL_ONOFF:
+                case VAL_ENUM:
+                    break;
+                default:
+                case VAL_NONE:
+                    break;
+            }
+        }
+    }
 }
 
 @end
